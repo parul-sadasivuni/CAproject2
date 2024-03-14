8063542bb9439c443a56245bbc41bae16b7f3bca
parul-sadasivuni <txpsree@gmail.com>
2024-03-14 00:23:11 -0500

    memory-addressed labels

--- project2/assembler.c
+++ project2/assembler.c
@@ -10,6 +10,136 @@ struct label {
     char* labelName;
 };
 
+struct code {
+    char type[7];
+    int8_t byte;
+    short shor;
+    int ints;
+    long long lon;
+    float flo;
+    double dou;
+};
+
+struct data {
+    char type[7];
+    int8_t byte;
+    short shor;
+    int ints;
+    long long lon;
+    float flo;
+    double dou;
+};
+
+void resetData(struct data dat) {
+    dat.type = NULL;
+    dat.byte = NULL;
+    dat.shor = NULL;
+    dat.ints = NULL;
+    dat.lon = NULL;
+    dat.flo = NULL;
+    dat.dou = NULL;
+}
+
+void resetInstr(struct code co) {
+    co.type = NULL;
+    co.byte = NULL;
+    co.shor = NULL;
+    co.ints = NULL;
+    co.lon = NULL;
+    co.flo = NULL;
+    co.dou = NULL;
+}
+
+bool checkData(struct data dat, char str[], int offset) {
+    if(strcmp(dat.type, "byte") == 0) {
+        int64_t foo = atoll(str);
+        if(foo >= INT8_MIN && foo <= INT8_MAX) {
+            int8_t foos = int8_t(foo);
+            dat.byte = foos;
+            return true;
+            offset += 1;
+        }
+        else { 
+            return false;
+        }
+    }
+    else if(strcmp(dat.type, "ascii") == 0) {
+        int8_t foo = str[0];
+        offset += 1;
+        return true;
+    }
+    else if(strcmp(dat.type, "short") == 0) {
+        int64_t foo = atoll(str);
+        if(foo >= INT16_MIN && foo <= INT16_MAX) {
+            short foos = short(foo);
+            dat.shor = foos;
+            offset += 2;
+            return true;
+        }
+        else { 
+            return false;
+        }
+    }
+    else if(strcmp(dat.type, "int") == 0) {
+        int64_t foo = atoll(str);
+        if(foo >= INT32_MIN && foo <= INT32_MAX) {
+            int foos = int(foo);
+            dat.ints = foos;
+            offset += 4;
+            return true;
+        }
+        else { 
+            return false;
+        }
+    }
+    else if(strcmp(dat.type, "long") == 0) {
+        int64_t foo = atoll(str);
+        if(foo >= INT64_MIN && foo <= INT64_MAX) {
+            short foos = short(foo);
+            dat.shor = foos;
+            offset += 8;
+            return true;
+        }
+        else { 
+            return false;
+        }
+    }
+    else if(strcmp(dat.type, "float") == 0) {
+        float foo = atof(str);
+        if(foo >= -__FLT_MAX__ && foo <= __FLT_MAX__) {
+            dat.flo = foo;
+            offset += 4;
+            return true;
+        }
+        else { 
+            return false;
+        }
+    }
+    else if(strcmp(dat.type, "double") == 0) {
+        double foo = atof(str);
+        if(foo >= -__DBL_MAX__ && foo <= __DBL_MAX__) {
+            dat.dou = foo;
+            offset += 8;
+            return true;
+        }
+        else { 
+            return false;
+        }
+    }
+    else {
+        return false;
+    }
+}
+
+int duplicateExists(struct label labelArray[], char* str, int size) {
+    for(int i = 0; i < size; i++) {
+        if (strcmp(labelArray[i].labelName, str) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
 int main(int argc, char** argv) {
     //addresses are 24 bit (unsigned, BIG ENDIAN) or operations with labels
     //first 4 bytes of slko are offset of .code, second 4 bytes are offset of .data section (8+P)
@@ -53,9 +183,9 @@ int main(int argc, char** argv) {
         //pushl value 03
         "^\tpushl (-?[0-9]{1,19})\n$", //64 bit integer
         //pushf value 04
-        "^\tpushf (\d*\.?\d*)\n$", //32 bit float //TODO: can be negative?
+        "^\tpushf (\\d*\\.?\\d*)\n$", //32 bit float 
         //pushd value 05
-        "^\tpushd (\d*\.?\d*)\n$", //64 bit double 
+        "^\tpushd (\\d*\\.?\\d*)\n$", //64 bit double 
         //pushbm address 06
         "^\tpushbm ([0-9]{1,8}|:([a-zA-Z0-9_-]+))\n$", //address will be 24 bit, operation will be 8 bit
         //pushsm address 07
@@ -64,10 +194,10 @@ int main(int argc, char** argv) {
         "^\tpushim ([0-9]{1,8}|:([a-zA-Z0-9_-]+))\n$", //32 
         //pushlm address 09
         "^\tpushlm ([0-9]{1,8}|:([a-zA-Z0-9_-]+))\n$", //64
-        //pushfm address 0a //TODO fix this regex
-        "^\tpushfm (\d*\.?\d*|:([a-zA-Z0-9_-]+))\n$", //32 float
+        //pushfm address 0a //TODO verify if this actually works buddy
+        "^\tpushfm (\\d*\\.?\\d*|:([a-zA-Z0-9_-]+))\n$", //32 float
         //pushdm address 0b
-        "^\tpushdm (\d*\.?\d*|:([a-zA-Z0-9_-]+))\n$", //64 float
+        "^\tpushdm (\\d*\\.?\\d*|:([a-zA-Z0-9_-]+))\n$", //64 float
         //pushmm address items 0c
         "^\tpushmm ([0-9]{1,8}|:([a-zA-Z0-9_-]+)) ([0,9]{1,3})\n$", //items is 8 bit, operation is 8 bit
         //dupb 0e
@@ -103,9 +233,9 @@ int main(int argc, char** argv) {
         //poplm address 1c
         "^\tpoplm ([0-9]{1,8}|:([a-zA-Z0-9_-]+))\n$", //64 bit integer
         //popfm address 1d 
-        "^\tpopfm (\d*\.?\d*|:([a-zA-Z0-9_-]+))\n$", //32 bit float 
+        "^\tpopfm (\\d*\\.?\\d*|:([a-zA-Z0-9_-]+))\n$", //32 bit float 
         //popdm address 1e 
-        "^\tpopdm (\d*\.?\d*|:([a-zA-Z0-9_-]+))\n$", //64 bit float
+        "^\tpopdm (\\d*\\.?\\d*|:([a-zA-Z0-9_-]+))\n$", //64 bit float
         //popmm address items 1f
         "^\tpopmm ([0-9]{1,8}|:([a-zA-Z0-9_-]+) ([0,9]{1,3}))\n$", //items is 8 bit
         //swapb 20  
@@ -307,7 +437,7 @@ int main(int argc, char** argv) {
         //shftll value 82
         "^\tshftll (-?[0-9]{1,3})\n$", //64 bit
         //jmp address 83
-        "^\tjmp ([0-9]{1,8}|:([a-zA-Z0-9_-]+))\n$", //TODO CHECK WHETHER OPERATIONS ARE LEGAL (im p sure they are)
+        "^\tjmp ([0-9]{1,8}|:([a-zA-Z0-9_-]+))\n$", 
         //jrpc offset 84
         "^\tjrpc (-?[0-9]{1,3})\n$", //signed 8 bit //TODO MAKE SURE CANT HAVE LABEL HERE
         //jind 85
@@ -330,7 +460,7 @@ int main(int argc, char** argv) {
         "^\treturn\n$",
         //halt 8e
         "^\thalt\n$",
-        //labels 143 //TODO ADD IN LABELS W OPERATION 
+        //labels 143 //TODO ADD IN LABELS W OPERATION FOR INSTRUCTIONS 
         "^:([a-zA-Z0-9_-]+\n$)",
         //.code 144
         "^.code\n$",
@@ -350,20 +480,21 @@ int main(int argc, char** argv) {
         "^.float\n$",
         //.double 152
         "^.double\n$",
+        //any other number data type 153
+        "\t[0-9]+\\.?[0-9]\n$",
+        //ascii data type 154
+        "^\t[a-zA-Z]\n$"
     };
 
     //pass 1: counting labels
     char str[100];
     int countLabels = 0;
+    int countCode = 0;
+    int countData = 0;
     bool hasCode = false;
     bool codeSection = false;
     bool dataSection = false;
-    // int lineNum = 0;
     while(fgets(str, sizeof(str), file) != NULL) {
-        // if(lineNum == 0 && strcmp(str, ".code\n") != 0) { //TODO check whether it has to start with .code or just has to have one in there?
-        //     fprintf(stderr, "Error on line 0\n");
-        //     exit(1);
-        // }
         if(str[0] == ':') {
             countLabels++;
         }
@@ -371,13 +502,22 @@ int main(int argc, char** argv) {
             hasCode = true;
             codeSection = true;
             dataSection = false;
+            typ = NULL;
         }
-        if(strcmp(str, ".data") == 0) {
+        else if(strcmp(str, ".data") == 0) {
             hasCode = true;
             codeSection = false;
             dataSection = true;
         }
-        //lineNum++;
+        else if(strcmp(str, ".byte") == 0 || strcmp(str, ".ascii") == 0 || strcmp(str, ".short") == 0 || strcmp(str, ".int") == 0 || strcmp(str, ".long") == 0 || strcmp(str, ".float") == 0 || strcmp(str, ".double") == 0 || str[0] == ';') {
+            continue;
+        }
+        if(codeSection) {
+            countCode++;
+        }
+        else if(dataSection) {
+            countData++;
+        }
     }
     //make sure at least 1 .code
     if(!hasCode) {
@@ -388,12 +528,12 @@ int main(int argc, char** argv) {
         
     //pass 2: adding in offsets
     struct label labelsArray[countLabels]; //created a label array
-
-    //part 1: going through and assigning flag to help identify size of instruction (1 byte for opcode + size of operand)
+    struct code instrArray[countCode]; //array for instr
+    struct data dataArray[countData]; //array for data
 
     //compiling regex
-    regex_t regEx[41];
-    for(int i = 0; i < 41; i++) {
+    regex_t regEx[155];
+    for(int i = 0; i < 155; i++) {
         if (regcomp(&regEx[i], regexInstructions[i], REG_EXTENDED) != 0) {
             fprintf(stderr, "Could not compile regex pattern: %s\n", regexInstructions[i]);
             return 1;
@@ -401,27 +541,20 @@ int main(int argc, char** argv) {
     }
 
     //code, data types of line
-    bool code = false; //all after contain instructions
-    bool data = false; //all after contain data
-    bool byte = false;
-    bool ascii = false; //all after contain type ascii: signed 8 bit
-    bool shor = false; //short: signed 16 bit
-    bool ints = false; //int: signed 32 bit
-    bool lon = false; //long: signed 64 bit 
-    bool flo = false; //float: 32 bit float 
-    bool dou = false; //double: 64 bit float
+    codeSection = false; //all after contain instructions
+    dataSection = false; //all after contain data
+    //byte: signed 8 bit
+    //ascii: signed 8 bit
+    //short: signed 16 bit
+    //int: signed 32 bit
+    //long: signed 64 bit 
+    //float: 32 bit float 
+    //double: 64 bit float
 
-    //instruction sizes
-    bool oneb = false;
-    bool twob = false;
-    bool threeb = false;
-    bool fourb = false;
-    bool eightb = false;
     int offset = 0;
     int lineNum = 1;
-    //TODO: check ranges here for the operands
     while(fgets(str, sizeof(str), file) != NULL) {
-        //identifying which instruction it is 
+        //identifying which line type it is 
         if(strncmp(str, ";", 1) != 0) { //ignoring if its a commment
             int value = -1;
             for (int i = 0; i < 41; i++) {
@@ -431,8 +564,9 @@ int main(int argc, char** argv) {
                 }
             }
             
+            //making sure instructions are in .code section
             if(value >= 0 && value <= 142) {
-                if(!code) {
+                if(!codeSection) {
                     fprintf(stderr, "Error on line %d", lineNum);
                     exit(1);
                 }
@@ -440,85 +574,140 @@ int main(int argc, char** argv) {
                 //TODO convert opcode to 1 byte binary but should i even do it here
             }
 
-            //assigning boolean flags based on what kind of instruction it is
+            //creating a struct based on instruction type
+            struct code co;
+            struct data dat;
+            char typ[7]; //to hold type of data
+            //separate offsets
+            int codeOffset = 0; 
+            int dataOffset = 0;
+            //index for the arrays
+            int datIndex = 0;
+            int codeIndex = 0;
+
+            int labelArrayIndex = 0;
             switch(value) {
+                //directives
                 case 144: 
                     code = true;
+                    data = false;
                     break;
                 case 145: 
                     data = true;
                     code = false;
                     break;
+                //subdirectives (sets type for data to follow)
                 case 146:
-                    byte = true;
-                    ascii = false;
-                    shor = false;
-                    ints = false;
-                    lon = false;
-                    flo = false;
-                    dou = false;
+                    strcpy(typ, "byte");
                     break;
                 case 147:
-                    ascii = true;
-                    byte = false;
-                    shor = false;
-                    ints = false;
-                    lon = false;
-                    flo = false;
-                    dou = false;
+                    strcpy(typ, "ascii");
                     break;
                 case 148:
-                    shor = true;
-                    byte = false;
-                    ascii = false;
-                    ints = false;
-                    lon = false;
-                    flo = false;
-                    dou = false;
+                    strcpy(typ, "short");
                     break;
                 case 149:
-                    ints = true;
-                    byte = false;
-                    ascii = false;
-                    shor = false;
-                    lon = false;
-                    flo = false;
-                    dou = false;
+                    strcpy(typ, "int");
                     break;
                 case 150:
-                    lon = true;
-                    byte = false;
-                    ascii = false;
-                    shor = false;
-                    ints = false;
-                    flo = false;
-                    dou = false;
+                    strcpy(typ, "long");
                     break;
                 case 151:
-                    flo = true;
-                    byte = false;
-                    ascii = false;
-                    shor = false;
-                    ints = false;
-                    lon = false;
-                    dou = false;
+                    strcpy(typ, "float");
                     break;
                 case 152:
-                    dou = true;
-                    byte = false;
-                    ascii = false;
-                    shor = false;
-                    ints = false;
-                    lon = false;
-                    flo = false;
+                    strcpy(typ, "double");
                     break;
-                case 0: case 123: 
-                    oneb = true;
+                //handling number data
+                case 153:
+                    //need to make sure its not ascii
+                    if(typ == NULL || strcmp(typ, "ascii") == 0) {
+                        fprintf(stderr, "Error on line %d", lineNum);
+                        exit(1);
+                    }
+
+                    dat.type = typ;
+                    //confirming that it is formatted correctly
+                    if(!checkData(dat, str, dataOffset)) {
+                        fprintf(stderr, "Error on line %d", lineNum);
+                        exit(1);
+                    }
+                    dataArray[datIndex] = dat; //assign to array (with initialized data type)
+                    datIndex++;
+                    resetData(dat); //reset struct
                     break;
+                //handling ascii
+                case 154: 
+                    //need to make sure it is ascii
+                    if(typ == NULL || strcmp(typ, "ascii") != 0) {
+                        fprintf(stderr, "Error on line %d", lineNum);
+                        exit(1);
+                    }
+
+                    dat.type = typ;
+                    //confirming formatting
+                    if(!checkData(dat, str, dataOffset)) {
+                        fprintf(stderr, "Error on line %d", lineNum);
+                        exit(1);
+                    }
+                    dataArray[datIndex] = dat; //assign to array
+                    datIndex++;
+                    resetData(dat); //reset struct
+                    break;
+                //instructions of various sizes
+                //8 bit
+                case 0: case 123: case 124: case 125: case 126: case 127: case 128: case 129: case 130: case 132: 
+                    codeOffset += 2; //1 byte + 1 byte
+                    break;
+                //16 bit 
+                case 1: 
+                    codeOffset += 3; //1 + 2 bytes
+                    break;
+                //case 24 bit
+                case 6: case 7: case 8: case 9: case 10: case 11: case 25: case 26: case 27: case 28: case 29: case 30: case 31: case 131: case 134: case 135: case 136: case 137: case 138: case 139: case 140:
+                    codeOffset += 4; //1 + 3 
+                //32 bit
+                case 2: case 4: 
+                    codeOffset += 5; //1 + 4
+                    break; 
+                //64 bit
+                case 3: case 5: 
+                    codeOffset += 9; //1 + 8
+                    break;
+                //24 bit + 8 bit items
+                case 12: case 31:
+                    codeOffset += 5; //1 + 3 + 1
+                    break;
+                //no operands
+                case 13: case 14: case 15: case 16: case 17: case 18: case 19: case 20: case 21: case 22: case 23: case 24: case 32: case 33: case 34: case 35: case 36: case 37: case 38: case 39: case 40: case 41: case 42: case 43: case 44: case 45: case 46: case 47: case 48: case 49: case 50: case 51: case 52: case 53: case 54: case 55:  case 57: case 58: case 59: case 60: case 61: case 62: case 63: case 64: case 65: case 66: case 67: case 68: case 69: case 70: case 71: case 72: case 73: case 74: case 75: case 76: case 77: case 78: case 79: case 80: case 81: case 82: case 83: case 84: case 85: case 86: case 87: case 88: case 89: case 90: case 91: case 92:  case 93: case 94: case 95: case 96: case 97: case 98: case 99: case 100: case 101: case 102: case 103: case 104: case 105: case 106: case 107: case 108: case 109: case 110: case 111: case 112: case 113: case 114: case 115: case 116: case 117: case 118: case 119: case 120: case 121: case 122: case 133: case 141: case 142: 
+                    codeOffset += 1; //1 byte + 0 operands
+                    break;
+                //label
+                case 143: 
+                    struct label tempLabel;
+                    tempLabel.labelName = strdup(str + 1);
+                    int dupe = duplicateExists(labelsArray, tempLabel.labelName, countLabels);
+                    if(dupe == 0) {
+                        if(codeSection) {
+                            tempLabel.memoryAddress = codeOffset;    
+                        }
+                        else if(dataSection) {
+                            tempLabel.memoryAddress = 0x10000 + dataOffset;
+                        }
+                        labelsArray[labelArrayIndex] = tempLabel;
+                        labelArrayIndex++;
+                    }
+                    else {
+                        fprintf(stderr, "Error on line %d\n", lineNum);
+                        exit(1);
+                    }
+                    break;
+                default: 
+                    fprintf(stderr, "Error on line %d\n", lineNum);
+                    exit(1);
             }
         } 
         lineNum++;
-        //okay so now based on the flag, write to the file do i need to separate this to a different pass actually yes the writing part definitely has to be its own part because i need to do all the data first and then the code part so then maybe i assign to an array but is the array dynamically resizing?????? maybe i should do this while i go through the first time to like determine the size of each section
     }
     rewind(file);
 }
